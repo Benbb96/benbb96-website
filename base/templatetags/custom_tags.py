a62c159dc12f624508cbbf6ca9011eec81663ecf
bbe <benjamin.bernard-bouissieres@ipexia.com>
2018-11-08 12:31:16 +0100

    Squashed commit of the following:
    commit 18f098d13b7f7fa40f3d62f86fc26da32e3abb37
    Author: bbe <benjamin.bernard-bouissieres@ipexia.com>
    Date:   Thu Nov 8 12:30:06 2018 +0100
    
        Fixe la taille pour les caroussels d'image d'avis sur le détail d'un plat.
        Et donne aussi la note moyenne des restos avec une petite bar de progression sympa !
    
    commit 70edc6dfbc3995af86e508cb5da58b4a71152e7d
    Author: bbe <benjamin.bernard-bouissieres@ipexia.com>
    Date:   Thu Nov 8 12:03:58 2018 +0100
    
        Correct firebase image upload in admin inline (#40)

---
+++ base/templatetags/custom_tags.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def multiply_10(value):
+    return int(float(value) * 10)
+
+@register.filter
+def color(value):
+    if value >= 8:
+        return 'success'
+    if value >= 6:
+        return 'info'
+    if value >= 4:
+        return 'warning'
+    return 'danger'
