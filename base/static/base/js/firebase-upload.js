70edc6dfbc3995af86e508cb5da58b4a71152e7d
bbe <benjamin.bernard-bouissieres@ipexia.com>
2018-11-08 12:03:58 +0100

    Correct firebase image upload in admin inline (#40)

--- base/static/base/js/firebase-upload.js
+++ base/static/base/js/firebase-upload.js
@@ -1,4 +1,4 @@
-var config = {
+let config = {
     apiKey: "AIzaSyBZvRl1Q35AH9j4vYKwTM5YYMUZp6HAjLo",
     authDomain: "eminent-airport-148108.firebaseapp.com",
     databaseURL: "https://eminent-airport-148108.firebaseio.com",
@@ -8,29 +8,35 @@ var config = {
 };
 
 firebase.initializeApp(config);
-var storage = firebase.storage();
+let storage = firebase.storage();
 
-var img = document.getElementById('display');
-if (img.dataset.url !== 'None') {
-    var imagesRef = storage.ref(img.dataset.url);
-    imagesRef.getDownloadURL().then(function(url) {
-        img.src = url;
-    }).catch(function(error) {
-        alert('Erreur :' + error);
-    });
+// Load all images from Firebase
+let images = document.getElementsByClassName('display');
+for(let i=0; i < images.length; i++) {
+    let image = images[i];
+    if (image.dataset.url !== 'None') {
+        let imagesRef = storage.ref(image.dataset.url);
+        imagesRef.getDownloadURL().then(function(url) {
+            image.src = url;
+        }).catch(function(error) {
+            alert('Erreur :' + error.message);
+        });
+    }
 }
 
-function uploadImage(folder) {
+function uploadImage(value, folder) {
     // Show the progress bar
-    document.getElementById("progressbar").style.display = "inline";
+    let progressbar = document.getElementById(value).querySelectorAll("progress")[0];
+    console.log(progressbar);
+    progressbar.style.display = "inline";
 
-    var file = document.getElementById("files").files[0];
-    var storageRef = storage.ref();
-    var date = new Date();
-    var path = 'media/' + folder + '/' + date.getFullYear() + '/' + (date.getMonth() +1)  + '/' + date.getDate() + '/' + file.name;
-    var thisref = storageRef.child(path).put(file);
+    let file = document.getElementById(value).querySelectorAll("input[type=file]")[0].files[0];
+    let storageRef = storage.ref();
+    let date = new Date();
+    let path = 'media/' + folder + '/' + date.getFullYear() + '/' + (date.getMonth() +1)  + '/' + date.getDate() + '/' + file.name;
+    let thisref = storageRef.child(path).put(file);
     thisref.on('state_changed', function (snapshot) {
-        document.getElementById("progressbar").value = (snapshot.bytesTransferred / snapshot.totalBytes) * 100 ;
+        progressbar.value = (snapshot.bytesTransferred / snapshot.totalBytes) * 100 ;
         switch (snapshot.state) {
             case firebase.storage.TaskState.PAUSED: // or 'paused'
                 console.log('Upload is paused');
@@ -40,14 +46,14 @@ function uploadImage(folder) {
                 break;
         }
     }, function (error) {
-        alert("Une erreur s'est produite : " + error)
+        alert("Une erreur s'est produite : " + error.message)
     },
     function () {
         // Upload completed successfully, now we can get the download URL
         thisref.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-            document.getElementById("progressbar").style.display = "none";
-            document.getElementById("url").value = path;
-            document.getElementById("display").src = downloadURL;
+            progressbar.style.display = "none";
+            document.getElementById(value).querySelectorAll("input[type=text]")[0].value = path;
+            document.getElementById(value).querySelectorAll("img")[0].src = downloadURL;
         });
     });
 }
