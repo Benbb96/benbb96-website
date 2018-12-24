0d8e5555f9b07d60b88d182580f3072fd088557d
Benbb96 <benbb96@gmail.com>
2018-12-24 17:53:42 +0100

    Login & Logout pages.

--- benbb96/settings.py
+++ benbb96/settings.py
@@ -96,6 +96,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
--- benbb96/urls.py
+++ benbb96/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from benbb96 import settings
 
@@ -9,5 +10,7 @@ urlpatterns = [
     path('', include('base.urls')),
     path('avis/', include('avis.urls')),
     path('tracker/', include('tracker.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
