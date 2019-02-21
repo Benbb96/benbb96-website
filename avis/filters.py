d99f2a52047a057a53f323d113998edc07596781
Benbb96 <benbb96@gmail.com>
2019-02-21 23:04:27 +0100

    filters

--- avis/filters.py
+++ avis/filters.py
@@ -1,13 +1,27 @@
 import django_filters
 
-from avis.models import Structure
+from avis.models import Structure, TypeStructure, Produit, CategorieProduit
 
 
 class StructureFilter(django_filters.FilterSet):
+    nom = django_filters.CharFilter(lookup_expr='icontains', label='Nom')
+    informations = django_filters.CharFilter(lookup_expr='icontains', label='Informations')
+    type = django_filters.ModelChoiceFilter(
+        lookup_expr='exact', queryset=TypeStructure.objects.all(), label='Type structure',
+    )
+
     class Meta:
         model = Structure
-        fields = {
-            'nom': ['icontains'],
-            'informations': ['icontains'],
-            'type': ['exact']
-        }
+        fields = ('nom', 'informations', 'type')
+
+
+class ProduitFilter(django_filters.FilterSet):
+    nom = django_filters.CharFilter(lookup_expr='icontains', label='Nom')
+    categories = django_filters.ModelMultipleChoiceFilter(
+        lookup_expr='exact', label='Catégories', queryset=CategorieProduit.objects.all()
+    )
+    prix = django_filters.RangeFilter()
+
+    class Meta:
+        model = Produit
+        fields = ('nom', 'categories', 'prix')
