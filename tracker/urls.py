a01e109cb75312f579bbff5b0803ff6d17265b53
Benbb96 <benbb96@gmail.com>
2019-09-08 18:08:37 +0200

    Mets à jour l'historique en fonction de la période (mais l'édition/suppression ne fonctionne plus !) Commence à faire la mise à jour des graphs de répartition. A traiter dans le javascript. WIP

--- tracker/urls.py
+++ tracker/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('<int:pk>/update', views.TrackerUpdateView.as_view(), name='update-tracker'),
     path('<int:pk>/delete', views.TrackerDeleteView.as_view(), name='delete-tracker'),
     path('get/data/', views.tracker_data, name='tracker-data'),
+    path('get/get_other_stats/', views.get_other_stats, name='tracker-other-stats'),
+    path('get/history/', views.tracker_history, name='tracker-history'),
     path('track/<int:pk>/update/', views.TrackUpdateView.as_view(), name='update-track'),
     path('track/<int:pk>/delete/', views.TrackDeleteView.as_view(), name='delete-track')
 ]
