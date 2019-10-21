4e3600a00fd9c38ce25f64ee0ac79004af5aaefc
bbe <benjamin.bernard-bouissieres@ipexia.com>
2019-10-21 15:55:15 +0200

    Configuration des mails

--- config/settings/base.py
+++ config/settings/base.py
@@ -167,3 +167,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     ]
 }
+
+# Configuration de l'envoi des mails
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = get_secret_setting('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = get_secret_setting('EMAIL_HOST_PASSWORD')
+
+ADMINS = [('Benbb96', 'benbb96@gmail.com')]
+EMAIL_SUBJECT_PREFIX = '[Benbb96] '
