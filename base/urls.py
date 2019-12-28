d71456b3875ef745ac1ae93cff43fc413c291a2c
Benbb96 <benbb96@gmail.com>
2019-12-28 18:02:34 +0100

    Début de l'intégration du jeu de labyrinthe avec p5.js

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,7 @@ app_name = 'base'
 urlpatterns = [
     path('', views.ProjetListView.as_view(), name='home'),
     path(_('about'), TemplateView.as_view(template_name='base/about.html'), name='about'),
+    path(_('labyrinthe-game'), TemplateView.as_view(template_name='base/labyrinthe_game.html'), name='labyrinthe-game'),
     path('rallye-des-colocs', TemplateView.as_view(template_name='base/rallye.html'), name='rallye'),
     path('gallery', TemplateView.as_view(template_name='base/gallery.html'), name='gallery'),
     path('profil/<str:slug>', views.UserDetailView.as_view(), name='profil'),
