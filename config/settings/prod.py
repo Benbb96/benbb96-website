25a8bbbce2540c8c5127e3e20ed32a83a9d81d3f
Benbb96 <benbb96@gmail.com>
2020-01-05 17:38:09 +0100

    Mise en place Mailgun

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -37,10 +37,6 @@ CSRF_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 
-# Configuration de l'envoi des mails
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = get_secret_setting('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = get_secret_setting('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
+MAILGUN_ACCESS_KEY = get_secret_setting('ACCESS-KEY')
+MAILGUN_SERVER_NAME = get_secret_setting('SERVER-NAME')
