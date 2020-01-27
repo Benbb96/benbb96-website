6d7e173948645467102f6102d0fc6cf7edd355b5
Benbb96 <benbb96@gmail.com>
2020-01-27 23:14:30 +0100

    Options de filtre avec intégration de la lib Select2

--- my_spot/views.py
+++ my_spot/views.py
@@ -1,14 +1,30 @@
+from django.contrib.auth.models import Group
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
+from my_spot.forms import SpotFilterForm
 from my_spot.models import Spot
 
 
-def map(request):
+def carte(request):
     if request.is_ajax():
+        spots = Spot.objects.visible_for_user(request.user)
+        # Récupère les paramètres et filtre les spots
+        visibilite = request.GET.get('visibilite', None)
+        if visibilite and visibilite != '0':
+            spots = spots.filter(visibilite=int(visibilite))
+        tags = request.GET.getlist('tags[]', None)
+        if tags:
+            tags = map(int, tags)
+            spots = spots.filter(tags__in=tags)
+        groupes = request.GET.getlist('groupes[]', None)
+        if groupes:
+            groupes = map(int, groupes)
+            spots = spots.filter(groupes__in=groupes)
+
         data = []
-        for spot in Spot.objects.visible_for_user(request.user):
+        for spot in spots:
             data.append({
                 'position': {
                     'lat': spot.position.latitude,
@@ -21,4 +37,6 @@ def map(request):
             })
         return JsonResponse({'spots': data})
 
-    return render(request, 'my_spot/map.html')
+    form = SpotFilterForm(groupes=Group.objects.filter(user=request.user))
+
+    return render(request, 'my_spot/map.html', {'form': form})
