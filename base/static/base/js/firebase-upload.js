4c3b93a67d0f34c91ecad0a794038b483c442696
Benbb96 <benbb96@gmail.com>
2020-02-14 21:54:07 +0100

    Amélioration du Widget Photo avec Firebase. Exemple de js handler dans l'admin pour utiliser l'évenement après l'ajout d'un nouvel élément inline.

--- base/static/base/js/firebase-upload.js
+++ base/static/base/js/firebase-upload.js
@@ -1,36 +1,29 @@
-let config = {
-    apiKey: "AIzaSyBZvRl1Q35AH9j4vYKwTM5YYMUZp6HAjLo",
+const config = {
+    apiKey: "AIzaSyC9uAZiNr9tAg4Y_Vc3xvlpFsCVBB2goEw",
     authDomain: "eminent-airport-148108.firebaseapp.com",
     databaseURL: "https://eminent-airport-148108.firebaseio.com",
     projectId: "eminent-airport-148108",
     storageBucket: "eminent-airport-148108.appspot.com",
-    messagingSenderId: "994857141623"
+    messagingSenderId: "994857141623",
+    appId: "1:994857141623:web:a3fcca47e0a59f0f984d72"
 };
 
 firebase.initializeApp(config);
 let storage = firebase.storage();
 
-// Load all images from Firebase
-let images = document.getElementsByClassName('display');
-for(let i=0; i < images.length; i++) {
-    let image = images[i];
-    if (image.dataset.url !== 'None') {
-        let imagesRef = storage.ref(image.dataset.url);
-        imagesRef.getDownloadURL().then(function(url) {
-            image.src = url;
-        }).catch(function(error) {
-            alert('Erreur :' + error.message);
-        });
-    }
-}
+function uploadImage() {
+    const parentDiv = this.parentElement;
+    const folder = this.getAttribute("data-folder");
 
-function uploadImage(value, folder) {
+    let file = parentDiv.querySelectorAll("input[type=file]")[0].files[0];
+    if (file === undefined) {
+        alert('Aucun fichier spécifié.');
+        return
+    }
     // Show the progress bar
-    let progressbar = document.getElementById(value).querySelectorAll("progress")[0];
-    console.log(progressbar);
+    let progressbar = parentDiv.querySelectorAll("progress")[0];
     progressbar.style.display = "inline";
 
-    let file = document.getElementById(value).querySelectorAll("input[type=file]")[0].files[0];
     let storageRef = storage.ref();
     let date = new Date();
     let path = 'media/' + folder + '/' + date.getFullYear() + '/' + (date.getMonth() +1)  + '/' + date.getDate() + '/' + file.name;
@@ -52,8 +45,30 @@ function uploadImage(value, folder) {
         // Upload completed successfully, now we can get the download URL
         thisref.snapshot.ref.getDownloadURL().then(function (downloadURL) {
             progressbar.style.display = "none";
-            document.getElementById(value).querySelectorAll("input[type=text]")[0].value = path;
-            document.getElementById(value).querySelectorAll("img")[0].src = downloadURL;
+            parentDiv.querySelectorAll("input[type=text]")[0].value = path;
+            parentDiv.querySelectorAll("img")[0].src = downloadURL;
         });
     });
 }
+
+$(document).ready(function() {
+    // Load all images from Firebase
+    let images = document.getElementsByClassName('display');
+    for(let i=0; i < images.length; i++) {
+        let image = images[i];
+        if (image.dataset.url !== 'None') {
+            let imagesRef = storage.ref(image.dataset.url);
+            imagesRef.getDownloadURL().then(function(url) {
+                image.src = url;
+            }).catch(function(error) {
+                alert('Erreur :' + error.message);
+            });
+        }
+    }
+
+    // Init upload image buttons
+    buttons = document.getElementsByClassName('uploadFirebaseImage');
+    Array.from(buttons).forEach(function(element) {
+        element.addEventListener('click', uploadImage);
+    });
+});
