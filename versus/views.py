02c94b15ec53640aa4c882789dee75adb55aaec4
Benbb96 <benbb96@gmail.com>
2020-03-18 23:26:57 +0100

    Affichage du top des joueurs pour un jeu. Et meilleur affichage de la liste des jeux. Optimisation des requêtes.

--- versus/views.py
+++ versus/views.py
@@ -11,8 +11,13 @@ class JeuListView(ListView):
 class JeuDetailView(DetailView):
     model = Jeu
     slug_field = 'slug'
+    queryset = Jeu.objects.select_related('createur__user').prefetch_related('parties__partiejoueur_set__joueur')
 
 
 class JoueurDetailView(DetailView):
     model = Joueur
     slug_field = 'slug'
+    queryset = Joueur.objects.select_related('profil__user').prefetch_related(
+        'partie_set__jeu', 'partie_set__partiejoueur_set__joueur',
+        'partiejoueur_set__partie__jeu', 'partiejoueur_set__partie__partiejoueur_set',
+    )
