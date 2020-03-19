70196493dd7fb16ab0d7bb1a11d997f6ca89db32
Benbb96 <benbb96@gmail.com>
2020-03-20 00:41:15 +0100

    API Tracker

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
-from tracker.models import Track
+from tracker.models import Tracker, Track
+
+
+class TrackerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tracker
+        fields = ('id', 'createur', 'nom', 'icone', 'color', 'date_creation', 'order')
 
 
 class TrackSerializer(serializers.ModelSerializer):
