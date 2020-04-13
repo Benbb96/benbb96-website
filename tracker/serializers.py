903fa49d7c9dd54f0119ef77e325cd011e2aeaa3
Benbb96 <benbb96@gmail.com>
2020-04-13 22:53:22 +0200

    Update serializers

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -3,16 +3,18 @@ from rest_framework import serializers
 from tracker.models import Tracker, Track
 
 
-class TrackerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tracker
-        fields = ('id', 'createur', 'nom', 'icone', 'color', 'date_creation', 'order')
-
-
 class TrackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Track
-        fields = ('tracker', 'datetime', 'commentaire')
+        fields = ('id', 'tracker', 'datetime', 'commentaire')
+
+
+class TrackerSerializer(serializers.ModelSerializer):
+    tracks = TrackSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Tracker
+        fields = ('id', 'createur', 'nom', 'icone', 'color', 'date_creation', 'order', 'tracks')
 
 
 class CustomTrackSerializer(serializers.ModelSerializer):
