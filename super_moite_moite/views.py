71878f5071c792fa551283ac311bab498326abe9
Benbb96 <benbb96@gmail.com>
2020-05-16 17:07:07 +0200

    Ajout des premières vues

--- super_moite_moite/views.py
+++ super_moite_moite/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from super_moite_moite.models import Logement
+
+
+class LogementListView(ListView):
+    context_object_name = 'logements'
+    model = Logement
+
+    def get_queryset(self):
+        return self.request.user.profil.logements.all()
+
+
+class LogementDetailView(DetailView):
+    context_object_name = 'logement'
+    model = Logement
+
+    def get_queryset(self):
+        return self.request.user.profil.logements.all()
