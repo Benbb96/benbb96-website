1290ba969436cd54996300e68d2ac448ede183c0
Benbb96 <benbb96@gmail.com>
2020-05-16 19:08:37 +0200

    Liste et création logement sur une function based view parce que c'est beaucoup plus simple ainsi !

--- super_moite_moite/forms.py
+++ super_moite_moite/forms.py
@@ -1,7 +1,41 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django_select2.forms import ModelSelect2MultipleWidget
 
+from base.models import Profil
 from base.widgets import FirebaseUploadWidget
-from super_moite_moite.models import Tache
+from super_moite_moite.models import Tache, Logement
+
+
+class LogementForm(forms.ModelForm):
+    class Meta:
+        model = Logement
+        fields = ('nom', 'habitants')
+        widgets = {
+            'nom': forms.TextInput(attrs={'class': 'form-control'}),
+            'habitants': ModelSelect2MultipleWidget(
+                model=Profil,
+                search_fields=[
+                    'user__username__icontains',
+                    'user__first_name__icontains',
+                    'user__last_name__icontains',
+                    'user__email__icontains'
+                ]
+            )
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.profil = kwargs.pop('profil', None)
+        super().__init__(*args, **kwargs)
+        self.initial = {'habitants': self.profil}
+
+    def clean_habitants(self):
+        habitants = self.cleaned_data['habitants']
+        if self.profil and self.profil not in habitants:
+            raise ValidationError(
+                'Vous devez vous inclure dans la liste des habitants pour avoir accès à ce logement.'
+            )
+        return habitants
 
 
 class TacheForm(forms.ModelForm):
--- super_moite_moite/urls.py
+++ super_moite_moite/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'super-moite-moite'
 
 urlpatterns = [
-    path('', views.LogementListView.as_view(), name='liste-logements'),
+    path('', views.liste_logements, name='liste-logements'),
     path('logement/<slug>', views.LogementDetailView.as_view(), name='detail-logement'),
 ]
