5dff2921eac11cb3db371189c462dfef5810c13d
Benbb96 <benbb96@gmail.com>
2020-06-28 12:08:43 +0200

    Refactor name contrast_color templatetag and add it to the Tracker serializer

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from base.templatetags.custom_tags import contrast_color
 from tracker.models import Tracker, Track
 
 
@@ -11,10 +12,14 @@ class TrackSerializer(serializers.ModelSerializer):
 
 class TrackerSerializer(serializers.ModelSerializer):
     tracks = TrackSerializer(many=True, read_only=True)
+    contrast_color = serializers.SerializerMethodField()
 
     class Meta:
         model = Tracker
-        fields = ('id', 'createur', 'nom', 'icone', 'color', 'date_creation', 'order', 'tracks')
+        fields = ('id', 'createur', 'nom', 'icone', 'color', 'contrast_color', 'date_creation', 'order', 'tracks')
+
+    def get_contrast_color(self, tracker):
+        return contrast_color(tracker.color)
 
 
 class CustomTrackSerializer(serializers.ModelSerializer):
