f919a37412af1bcf4c203e01864f000f39f94f89
Benbb96 <benbb96@gmail.com>
2020-06-30 23:48:19 +0200

    Ajout des listes de Tricks et des combos avec des filtres de recherche

--- kendama/views.py
+++ kendama/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django_filters.views import FilterView
 
-# Create your views here.
+from kendama.filters import KendamaTrickFliter, ComboFliter
+
+
+class KendamaTrickList(FilterView):
+    filterset_class = KendamaTrickFliter
+    context_object_name = 'tricks'
+
+
+class ComboList(FilterView):
+    filterset_class = ComboFliter
+    context_object_name = 'combos'
