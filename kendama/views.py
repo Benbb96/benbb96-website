d593ef9ea6a9baec9328e824204a9f924d7e72d5
Benbb96 <benbb96@gmail.com>
2020-07-01 00:04:41 +0200

    Ajout des pages de détail des tricks et combos

--- kendama/views.py
+++ kendama/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from django_filters.views import FilterView
 
 from kendama.filters import KendamaTrickFliter, ComboFliter
+from kendama.models import KendamaTrick, Combo
 
 
 class KendamaTrickList(FilterView):
@@ -9,6 +11,14 @@ class KendamaTrickList(FilterView):
     context_object_name = 'tricks'
 
 
+class KendamaTrickDetail(DetailView):
+    model = KendamaTrick
+
+
 class ComboList(FilterView):
     filterset_class = ComboFliter
     context_object_name = 'combos'
+
+
+class ComboDetail(DetailView):
+    model = Combo
