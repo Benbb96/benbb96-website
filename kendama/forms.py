a4adc34653a06416823c64660d4a18d07fccc2d6
Benbb96 <benbb96@gmail.com>
2020-07-04 13:33:29 +0200

    Page Update et Create de KendmaTrick. Librairie autoslug pour pas se faire chier sur la création.

--- kendama/forms.py
+++ kendama/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from base.widgets import FirebaseUploadWidget
-from kendama.models import Kendama, TrickPlayer, ComboPlayer
+from kendama.models import Kendama, TrickPlayer, ComboPlayer, KendamaTrick
 
 
 class KendamaForm(forms.ModelForm):
@@ -9,6 +9,15 @@ class KendamaForm(forms.ModelForm):
         model = Kendama
         fields = '__all__'
         widgets = {'photo': FirebaseUploadWidget(folder='kendamas')}
+
+
+class KendamaTrickForm(forms.ModelForm):
+    class Meta:
+        model = KendamaTrick
+        exclude = ('slug', 'creator', 'players')
+        widgets = {
+            'tutorial_video_link': forms.TextInput(attrs={'class': 'input-block'})
+        }
 
 
 class TrickPlayerForm(forms.ModelForm):
