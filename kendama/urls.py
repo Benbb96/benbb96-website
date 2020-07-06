9f248437b52350359107b69e75f8ced820dfb896
Benbb96 <benbb96@gmail.com>
2020-07-07 00:04:12 +0200

    Refactor la vue pour mettre à jour une fréquence de réussite

--- kendama/urls.py
+++ kendama/urls.py
@@ -11,20 +11,15 @@ urlpatterns = [
     path('tricks/create', views.KendamaTrickCreate.as_view(), name='create-trick'),
     path('tricks/<slug:slug>/update', views.KendamaTrickUpdate.as_view(), name='update-trick'),
     path('tricks/<slug:slug>/delete', views.KendamaTrickDelete.as_view(), name='delete-trick'),
-    path(
-        'tricks/<int:trick_id>/update-frequency',
-        views.update_trick_player_frequency,
-        name='update_trick_player_frequency'
-    ),
     path('combos/', views.ComboList.as_view(), name='combos'),
     path('combos/<slug:slug>/', views.ComboDetail.as_view(), name='detail-combo'),
     path('combos/create', views.ComboCreate.as_view(), name='create-combo'),
     path('combos/<slug:slug>/update', views.ComboUpdate.as_view(), name='update-combo'),
     path('combos/<slug:slug>/delete', views.ComboDelete.as_view(), name='delete-combo'),
     path(
-        'combos/<int:combo_id>/update-frequency',
-        views.update_combo_player_frequency,
-        name='update_combo_player_frequency'
+        '<str:cls>/<int:obj_id>/update-frequency',
+        views.update_player_frequency,
+        name='update_player_frequency'
     ),
     path('combos/create-trick', views.create_trick_from_modal, name='create-trick-from-modal'),
 
