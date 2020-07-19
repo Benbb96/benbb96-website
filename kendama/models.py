67c8f0b7167b82e1da229a111edefcb6740228e2
Benbb96 <benbb96@gmail.com>
2020-07-19 12:38:56 +0200

    Détail d'un kendama

--- kendama/models.py
+++ kendama/models.py
@@ -162,3 +162,6 @@ class Kendama(PhotoAbstract):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('kendama:detail-kendama', args=[self.slug])
