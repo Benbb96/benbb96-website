15b72a11f7c9ea3761b3e32e9b3c9b69595fe926
Benbb96 <benbb96@gmail.com>
2020-08-08 00:46:25 +0200

    Formulaire pour éditer ses informations de profil (fix #73)

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth.models import User
+
+from base.fields import MyDateField
+from base.models import Profil
 
 
 class SignUpForm(UserCreationForm):
@@ -11,3 +14,15 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+
+class ProfilForm(forms.ModelForm):
+    username = UsernameField(label="Nom d'utilisateur")
+    first_name = forms.CharField(max_length=30, label='Prénom', required=False)
+    last_name = forms.CharField(max_length=30, label='Nom', required=False)
+    email = forms.EmailField(max_length=254)
+    birthday = MyDateField(label='Date anniversaire', required=False)
+
+    class Meta:
+        model = Profil
+        fields = ('username', 'first_name', 'last_name', 'email', 'avatar', 'birthday')
