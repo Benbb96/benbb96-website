3df4ff009e4786d72c5c763ab9a8c4e5ad28982f
bbe <benjamin.bernard-bouissieres@ipexia.com>
2020-08-13 16:42:11 +0200

    Liste des joueurs et réorganisation des templates avec le titre du jumbotron.

--- versus/urls.py
+++ versus/urls.py
@@ -5,22 +5,23 @@ from . import views
 app_name = 'versus'
 
 urlpatterns = [
-    path('', views.JeuListView.as_view(), name='liste-jeux'),
-    path('<slug:slug>', views.JeuDetailView.as_view(), name='detail-jeu'),
-    path('<slug:slug>/ajout', views.ajout_partie, name='ajout-partie'),
+    path('jeux/', views.JeuListView.as_view(), name='liste-jeux'),
+    path('jeux/<slug:slug>', views.JeuDetailView.as_view(), name='detail-jeu'),
+    path('jeux/<slug:slug>/ajout', views.ajout_partie, name='ajout-partie'),
     path(
-        '<slug:slug>/edition/<int:partie_id>',
+        'jeux/<slug:slug>/edition/<int:partie_id>',
         views.edition_partie,
         name='edition-partie'
     ),
     path(
-        '<slug:slug>/suppression/<int:partie_id>',
+        'jeux/<slug:slug>/suppression/<int:partie_id>',
         views.suppression_partie,
         name='suppression-partie'
     ),
+    path('joueurs/', views.JoueurListView.as_view(), name='liste-joueurs'),
     path(
         'joueurs/<slug:slug>',
         views.JoueurDetailView.as_view(),
         name='detail-joueur'
-    )
+    ),
 ]
