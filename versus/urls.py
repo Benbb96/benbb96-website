9e1e5e1d69eedac20fd11cfcd64f71bddfd9d444
bbe <benjamin.bernard-bouissieres@ipexia.com>
2020-08-13 11:31:16 +0200

    Ajout et édition d'une partie d'un jeu. Mise à jour de l'affichage des parties de type Classement.

--- versus/urls.py
+++ versus/urls.py
@@ -8,5 +8,7 @@ app_name = 'versus'
 urlpatterns = [
     path('', views.JeuListView.as_view(), name='liste-jeux'),
     path('<slug:slug>', views.JeuDetailView.as_view(), name='detail-jeu'),
+    path('<slug:slug>/ajout', views.ajout_partie, name='ajout-partie'),
+    path('<slug:slug>/edition/<int:partie_id>', views.edition_partie, name='edition-partie'),
     path('joueurs/<slug:slug>', views.JoueurDetailView.as_view(), name='detail-joueur')
 ]
--- versus/views.py
+++ versus/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.forms import inlineformset_factory, NumberInput
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
+from django_select2.forms import ModelSelect2Widget
 
-from versus.models import Jeu, Joueur
+from versus.models import Jeu, Joueur, Partie, PartieJoueur
 
 
 class JeuListView(ListView):
@@ -21,3 +24,44 @@ class JoueurDetailView(DetailView):
         'partie_set__jeu', 'partie_set__partiejoueur_set__joueur',
         'partiejoueur_set__partie__jeu', 'partiejoueur_set__partie__partiejoueur_set',
     )
+
+
+PartieJoueurFormSet = inlineformset_factory(
+    Partie,
+    PartieJoueur,
+    fields=('joueur', 'score_classement'),
+    widgets={
+        'joueur': ModelSelect2Widget(
+            model=Joueur,
+            search_fields=['nom__icontains', 'profil__user__username__icontains'],
+            attrs={'style': 'width: 100%', 'data-minimum-input-length': 0}
+        ),
+        'score_classement': NumberInput(attrs={'class': 'form-control'})
+    },
+    can_delete=False
+)
+
+
+def ajout_partie(request, slug):
+    jeu = get_object_or_404(Jeu, slug=slug)
+    partie = Partie(jeu=jeu)
+    formset = PartieJoueurFormSet(request.POST or None, instance=partie)
+    if formset.is_valid():
+        partie.save()
+        formset.save()
+        messages.success(request, 'La nouvelle partie de %s a bien été ajouté.' % jeu)
+        return redirect(jeu)
+    return render(request, 'versus/partie_form.html', {'jeu': jeu, 'formset': formset})
+
+
+def edition_partie(request, slug, partie_id):
+    jeu = get_object_or_404(Jeu, slug=slug)
+    partie = get_object_or_404(Partie, id=partie_id)
+    formset = PartieJoueurFormSet(request.POST or None, instance=partie)
+    if formset.is_valid():
+        formset.save()
+        messages.success(request, 'La partie de %s a bien été mise à jour.' % jeu)
+        return redirect(jeu)
+    return render(request, 'versus/partie_form.html', {'jeu': jeu, 'partie': partie, 'formset': formset})
+
+
