04ee5129cf0af4cfae1c856412e33ccacb139dca
Benbb96 <benbb96@gmail.com>
2021-03-02 23:23:03 +0100

    Add slug to search field

--- music/admin.py
+++ music/admin.py
@@ -113,8 +113,8 @@ class MusiqueAdmin(SimpleHistoryAdmin):
     list_display_links = ('titre_display',)
     list_filter = ('styles', 'remixed_by')
     search_fields = (
-        'titre', 'artiste__nom_artiste', 'artiste__slug', 'remixed_by__nom_artiste',
-        'featuring__nom_artiste', 'album', 'label__nom'
+        'titre', 'slug', 'artiste__nom_artiste', 'artiste__slug', 'remixed_by__nom_artiste', 'remixed_by__slug'
+        'featuring__nom_artiste', 'featuring__slug', 'album', 'label__nom'
     )
     date_hierarchy = 'date_creation'
     ordering = ('-date_modification',)
